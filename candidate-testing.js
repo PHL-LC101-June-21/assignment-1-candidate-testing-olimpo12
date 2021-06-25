07fa60421dc8de6274bfad9fd0df9893bb926304
olimpo12 <85587431+olimpo12@users.noreply.github.com>
2021-06-25 16:47:03 -0400

    Update candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,45 +4,63 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 let candidateName="Jeff";
-
-////added value to candidateName
-
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question="Who was the first American woman in space?";
 let correctAnswer="Sally Ride";
-let candidateAnswer="Toby";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let candidateAnswer="Jonah";
+let questions= ["Who was the first American Woman in space?","What color is the moon?","What is the color of the sun?","Who was the first astronaut on the moon?", "Who was the 2nd astronaut on the moon?"];
+let correctAnswers= ["Sally Ride","White","Yellow","Neil Armstrong", "Buzz Aldrin"];
+let candidateAnswers=["Sally Ride", "Blue", "Yellow", "Neil Armstrong","Buzz Lightyear"];
 
-////defined variables
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 candidateName= input.question("What is your name?");
-
-  ////created question and updated variable candidateName value
 }
+
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer=input.question("Who was the first American woman in space?");
+
+
+candidateAnswer1= input.question(questions[1]);
+if(candidateAnswer1==correctAnswers[1]){console.log("True!");
+}
+else{console.log("False!");
 }
 
-////prompted user for answer and added variable to organize value
+candidateAnswer2= input.question(questions[2]);
+if(candidateAnswer2==correctAnswers[2]){console.log("True!");
+}
+else{console.log("False!");
+}
+
+candidateAnswer3= input.question(questions[3]);
+if(candidateAnswer3==correctAnswers[3]){console.log("True!");
+}
+else{console.log("False!");
+}
+candidateAnswer4= input.question(questions[4]);
+if(candidateAnswer4==correctAnswers[4]){console.log("True!");
+}
+else{console.log("False!");
+}
+
+candidateAnswer0=input.question("What is the name of the first Woman in space?");
+}
+
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-
-if(candidateAnswer== correctAnswer){console.log("True!");
+if(candidateAnswer0==correctAnswer){console.log("True!");
 }
 else{console.log("False!");
   }
-
-  ////responded to user's question with if, else statement
 }
+
+
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
@@ -50,6 +68,10 @@ function runProgram() {
 askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+
+
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
