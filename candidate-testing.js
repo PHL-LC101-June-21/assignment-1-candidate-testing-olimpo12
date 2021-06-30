b9a17530a94921ce99942b781976d03d69030cad
olimpo12 <85587431+olimpo12@users.noreply.github.com>
2021-06-30 18:43:38 -0400

    Update candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,6 +13,7 @@ let correctAnswers= ["Sally Ride","White","Yellow","Neil Armstrong", "Buzz Aldri
 let candidateAnswers=["Sally Ride", "Blue", "Yellow", "Neil Armstrong","Buzz Lightyear"];
 let dash= "-----------------------------";
 
+
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 candidateName= input.question("What is your name?");
@@ -23,38 +24,38 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
 
-candidateAnswer1= input.question(questions[1]);
-console.log("Your answer was: "+candidateAnswer1);
+candidateAnswers[1]= input.question(questions[1]);
+console.log("Your answer was: "+candidateAnswers[1]);
 
-if(candidateAnswer1==correctAnswers[1]){console.log("True!");
+if(candidateAnswers[1]==correctAnswers[1]){console.log("True!");
 }
 else{console.log("False!\nThe correct answer is: "+ correctAnswers[1]);
 }
 
 console.log(dash);
 
-candidateAnswer2= input.question(questions[2]);
-if(candidateAnswer2==correctAnswers[2]){console.log("True!");
+candidateAnswers[2]= input.question(questions[2]);
+if(candidateAnswers[2]==correctAnswers[2]){console.log("True!");
 }
 else{console.log("False!\nThe correct answer is: "+ correctAnswers[2]);
 }
 console.log(dash);
 
-candidateAnswer3= input.question(questions[3]);
-if(candidateAnswer3==correctAnswers[3]){console.log("True!");
+candidateAnswers[3]= input.question(questions[3]);
+if(candidateAnswers[3]==correctAnswers[3]){console.log("True!");
 }
 else{console.log("False!\nThe correct answer is: "+ correctAnswers[3]);
 }
 console.log(dash);
 
-candidateAnswer4= input.question(questions[4]);
-if(candidateAnswer4==correctAnswers[4]){console.log("True!");
+candidateAnswers[4]= input.question(questions[4]);
+if(candidateAnswers[4]==correctAnswers[4]){console.log("True!");
 }
 else{console.log("False!\nThe correct answer is: "+ correctAnswers[4]);
 }
 console.log(dash);
 
-candidateAnswer0=input.question("What is the name of the first Woman in space?");
+candidateAnswers[0]=input.question("What is the name of the first Woman in space?");
 }
 
 
@@ -62,10 +63,11 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-if(candidateAnswer0==correctAnswer){console.log("True!");
+if(candidateAnswers[0]==correctAnswer){console.log("True!");
 }
 else{console.log("False!\nThe correct answer is: "+ correctAnswer);
 }
+
 }
 
 
@@ -75,9 +77,41 @@ function runProgram() {
   console.log("Greetings "+candidateName+"!");
   console.log(dash);
 askQuestion();
+
+
   gradeQuiz(this.candidateAnswers);
+
+
+let correctResponses= Number(0);
+
+if (candidateAnswers[1] === correctAnswers[1])
+{correctResponses = correctResponses +1;
+}
+if (candidateAnswers[2] === correctAnswers[2])
+{correctResponses = correctResponses +1;
+}
+if (candidateAnswers[3] === correctAnswers[3])
+{correctResponses = correctResponses +1;
+}
+if (candidateAnswers[4] === correctAnswers[4])
+{correctResponses = correctResponses +1;
+}
+if (candidateAnswers[5] === correctAnswers[5])
+{correctResponses = correctResponses +1;
 }
 
+let percentage = correctResponses/questions.length*100;
+
+{console.log("Final Grade: "+ (percentage));
+}
+
+if (percentage > 70) {
+console.log("You pass!")
+} 
+else {
+console.log("Failing Grade.")
+}
+}
 
 
 
